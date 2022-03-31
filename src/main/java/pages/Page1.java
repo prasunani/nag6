3d2b0f1e56a4feff0e5604cfdc6e939f6cd39cf9
Vinla <Vinla@LAPTOP-M2UO22U7.roc.mn.charter.com>
2022-03-30 20:23:39 -0500

    Hi sowjanya, it is my second commit as a manager

---
+++ src/main/java/pages/Page1.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class Page1
+{
+	public static void main(String[] args) 
+	{
+		String x="ufbukfhbk#$^784FHJ";
+		String y=x.replaceAll("[^0-9]","");
+		System.out.println(y);
+
+	}
+
+}
+
+
